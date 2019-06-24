403a8b24dc00520f14c74e7da0d2f486199033ea
xing-yu <staryu.udi@gmail.com>
2019-06-23 23:38:13 -0400

    Create lab_station.sh

---
+++ lab_station.sh
@@ -0,0 +1,1 @@
+python3 ./source/word2vec.py /home/yu64/Documents/data/dic.json /home/yu64/Documents/data/idx2words.txt embedding.txt /home/yu64/Documents/data/pretrains_100d.txt 512 5 100 32 ./checkpoints 10000
