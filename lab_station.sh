43bbc77697c1673e84f4cfe8d408f330c7d48106
xing-yu <staryu.udi@gmail.com>
2019-06-24 12:45:41 -0400

    tune parameters

--- lab_station.sh
+++ lab_station.sh
@@ -1,1 +1,1 @@
-python3 ./source/word2vec.py /home/yu64/Documents/data/dic.json /home/yu64/Documents/data/idx2words.txt embedding.txt /home/yu64/Documents/data/pretrains_100d.txt 32 5 100 16 ./checkpoints 100000
+python3 ./source/word2vec.py /home/yu64/Documents/data/dic.json /home/yu64/Documents/data/idx2words.txt embedding.txt /home/yu64/Documents/data/pretrains_100d.txt 32 5 100 16 ./checkpoints 1000000
